bef1f2e3ec350197f5571c7c109df34e42d57754
SUMON <110353170+SumonDas5505@users.noreply.github.com>
2025-08-25 15:49:11 +0600

    Add navigation bar with sidebar configuration and styles

--- app.py
+++ app.py
@@ -2,6 +2,39 @@ import streamlit as st
 import matplotlib.pyplot as plt    
 import numpy as np
 import random
+from streamlit_navigation_bar import st_navbar
+
+
+st.set_page_config(initial_sidebar_state="collapsed")
+
+pages = ["Home", "Contributors"]
+styles = {
+    "nav": {
+        "background-color": "rgb(255, 75, 75)",
+    },
+    "div": {
+        "max-width": "32rem",
+    },
+    "span": {
+        "border-radius": "0.5rem",
+        "color": "rgb(49, 51, 63)",
+        "margin": "0 0.125rem",
+        "padding": "0.4375rem 0.625rem",
+    },
+    "active": {
+        "background-color": "rgba(255, 255, 255, 0.25)",
+    },
+    "hover": {
+        "background-color": "rgba(255, 255, 255, 0.35)",
+    },
+}
+
+page = st_navbar(pages, styles=styles)
+st.write(page)
+
+
+
+
 
 
 with st.sidebar:
